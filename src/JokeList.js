c1f7fea2823b29f20942a5a9ee060e0b29c5443e
lindajani <76737249+lindajani@users.noreply.github.com>
2021-01-17 16:46:00 +0200

    Created JokeList component

---
+++ src/JokeList.js
@@ -0,0 +1,13 @@
+import { Component } from "react";
+
+class JokeList extends Component {
+    render() {
+        return (
+            <div>
+<hi>Dad Jokes</hi>
+            </div>
+        )  
+
+    }
+}
+export default JokeList;
