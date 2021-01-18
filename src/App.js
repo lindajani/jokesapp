ae98cd1a49cff3278695f13a229c90b802ef2987
lindajani <76737249+lindajani@users.noreply.github.com>
2021-01-18 06:04:10 +0200

    My first ten Jokes

--- src/App.js
+++ src/App.js
@@ -5,8 +5,7 @@ import JokeList from './JokeList';
 function App() {
   return (
     <div className="App">
-      <JokeList />
-     
+      <JokeList />     
     </div>
   );
 }
--- src/JokeList.js
+++ src/JokeList.js
@@ -1,13 +1,30 @@
+import axios from "axios";
 import { Component } from "react";
 
 class JokeList extends Component {
+    static defaultProps = {
+        numJokesToGet: 10
+    };
+    constructor(props) {
+    super(props);
+    this.state = { jokes: [] };
+}
+    async componentDidMount()  {
+        // Load Jokes
+        let jokes = [];
+        while (jokes.length < this.props.numJokesToGet) {
+       let res = await axios.get("https://icanhazdadjoke.com", {headers: { Accept: "application/json"}
+    });
+    jokes.push(res.data.joke);
+}
+       console.log(jokes);
+    }
     render() {
-        return (
+        return ( 
             <div>
-<hi>Dad Jokes</hi>
+                <hi>Dad Jokes</hi>
             </div>
-        )  
-
+        );
     }
 }
 export default JokeList;
