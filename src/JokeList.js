02c3e165f94de24a1df19324d346532f1ca746f2
lindajani <76737249+lindajani@users.noreply.github.com>
2021-01-27 06:18:32 +0200

    Styling Joke Component

--- src/JokeList.js
+++ src/JokeList.js
@@ -16,14 +16,21 @@ class JokeList extends Component {
        let res = await axios.get("https://icanhazdadjoke.com", {headers: { Accept: "application/json"}
     });
     jokes.push(res.data.joke);
-}
+} 
+this.setState({ jokes: jokes });
        console.log(jokes);
     }
     render() {
         return ( 
-            <div>
+            <div className='JokeList'>
                 <hi>Dad Jokes</hi>
+            <div classname="JokeList-jokes">
+            {this.state.jokes.map(j => (
+                <div>{j}</div>
+            ))}
             </div>
+            </div>    
+            
         );
     }
 }
